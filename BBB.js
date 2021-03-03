5df2a85ac031761f7e4ece1525bc8e750632a502
jake3737 <50729332+jake3737@users.noreply.github.com>
2021-03-03 10:56:37 +0800

    Add files via upload

--- BBB.js
+++ BBB.js
@@ -2,7 +2,7 @@
 adwktt
 轉載備註名字
 打开App获取Cookie
-下載地址：http://bububao.yichengw.cn/?id=524855
+下載地址：http://bububao.yichengw.cn/?id=525644
 
 圈x
 [rewrite_local]
@@ -10,7 +10,7 @@ adwktt
 https://bububao.duoshoutuan.com/user/profile url script-request-header https://raw.githubusercontent.com/adwktt/adwktt/master/BBB.js
 
 [task_local]
-0 8-23/2 * * * https://raw.githubusercontent.com/adwktt/adwktt/master/BBB.js, tag=步步宝, 
+0 8-23/2 * * * https://raw.githubusercontent.com/adwktt/adwktt/master/BBB.js, tag=步步宝,
 
 loon
 [Script]
@@ -29,19 +29,19 @@ hostname = bububao.duoshoutuan.com,
 
 
 const $ = new Env('步步寶')
+const notify = $.isNode() ?require('./sendNotify') : '';
 let notice = ''
-//let CookieVal = $.getdata('bbb_ck')
-
-let CookieVal = process.env.BBBCK
+let CookieVal = $.getdata('bbb_ck')
 
 if ($.isNode()) {
+      CookieVal = process.env.BBB_ck.split()
       console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
       console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
 }
 
 
 
-now = new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000);  
+now = new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000);
 
 if (typeof $request !== 'undefined') {
    if ($request && $request.method != `OPTIONS` && $request.url.indexOf('user/profile') != -1) {
@@ -103,15 +103,22 @@ return new Promise((resolve, reject) => {
    $.post(userInfo,async(error, response, data) =>{
      const userinfo = JSON.parse(data)
      if(response.statusCode == 200 && userinfo.code != -1){
-$.log('\n🎉模擬登陸成功\n')
-     notice += '🎉步步寶帳號: '+userinfo.username+'\n'+'🎉當前金幣: '+userinfo.jinbi+'💰 約'+userinfo.money+'元💸\n'
+
+        $.log('\n🎉模擬登陸成功\n')
+        money = userinfo.money
+        nickname = userinfo.username
+        today_gold = userinfo.day_jinbi
+        notice += '🎉步步寶帳號: '+userinfo.username+'\n'+'🎉當前金幣: '+userinfo.jinbi+'🎉今日金幣: '+userinfo.day_jinbi+'💰 約'+userinfo.money+'元💸\n'
     }else{
-     notice += '⚠️異常原因: '+userinfo.msg+'\n'
+        notice += '⚠️異常原因: '+userinfo.msg+'\n'
            }
+     if (money >= 50 ){
+       notify.sendNotify(`${$.name}-账号${nickname} 余额${money}元已提现` , `账号${nickname} ${money}元已提现 \n今日收益为${today_gold}`)
+       }
           resolve()
     })
    })
-  } 
+  }
 
 
 function signIn() {
@@ -134,7 +141,7 @@ $.log('\n🔔開始签到\n')
           resolve()
     })
    })
-  } 
+  }
 
 function signDouble() {
 return new Promise((resolve, reject) => {
@@ -155,7 +162,7 @@ $.log('\n🔔開始領取每日觀看獎勵\n')
           resolve()
     })
    })
-  } 
+  }
 
 function zaoWanDkInfo() {
 return new Promise((resolve, reject) => {
@@ -175,7 +182,7 @@ return new Promise((resolve, reject) => {
           resolve()
     })
    })
-  } 
+  }
 
 
 
@@ -197,7 +204,7 @@ return new Promise((resolve, reject) => {
           resolve()
     })
    })
-  } 
+  }
 
 function dkClick() {
 return new Promise((resolve, reject) => {
@@ -220,7 +227,7 @@ return new Promise((resolve, reject) => {
           resolve()
     })
    })
-  } 
+  }
 
 
 function guaList() {
@@ -249,7 +256,7 @@ $.log('\n⚠️刮刮卡已用完,請明天再刮吧！\n')
           resolve()
     })
    })
-  } 
+  }
 
 function guaDet() {
 return new Promise((resolve, reject) => {
@@ -273,7 +280,7 @@ $.log('\nglid: '+GLID+'\n')
           resolve()
     })
    })
-  } 
+  }
 
 function guaPost() {
 return new Promise((resolve, reject) => {
@@ -295,7 +302,7 @@ $.log('\n🔔開始刮卡\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 function guaDouble() {
@@ -319,7 +326,7 @@ $.log('\n🔔開始領取刮卡翻倍獎勵\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 
@@ -349,7 +356,7 @@ $.log('\n🔔開始查詢喝水杯數\n')
           resolve()
     })
    })
-  } 
+  }
 
 function checkWaterSp() {
 return new Promise((resolve, reject) => {
@@ -368,7 +375,7 @@ return new Promise((resolve, reject) => {
           resolve()
     })
    })
-  } 
+  }
 
 function WaterSp() {
 return new Promise((resolve, reject) => {
@@ -388,7 +395,7 @@ return new Promise((resolve, reject) => {
           resolve()
     })
    })
-  } 
+  }
 
 function waterClick() {
 return new Promise((resolve, reject) => {
@@ -409,7 +416,7 @@ $.log('\n🔔開始領取喝水獎勵\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 function sleepStatus() {
@@ -448,7 +455,7 @@ now.getHours() >= 18){
           resolve()
     })
    })
-  } 
+  }
 
 
 
@@ -470,7 +477,7 @@ $.log('\n🔔開始睡覺\n')
           resolve()
     })
    })
-  } 
+  }
 
 function sleepEnd() {
 return new Promise((resolve, reject) => {
@@ -491,7 +498,7 @@ $.log('\n🔔開始起床\n')
           resolve()
     })
    })
-  } 
+  }
 
 function sleepDone() {
 return new Promise((resolve, reject) => {
@@ -512,7 +519,7 @@ $.log('\n🔔開始領取睡覺金幣\n')
           resolve()
     })
    })
-  } 
+  }
 
 function clickTaskStatus() {
 return new Promise((resolve, reject) => {
@@ -533,7 +540,7 @@ $.log('\n🔔開始查詢每日點擊任務狀態\n')
        resolve()
     })
    })
-  } 
+  }
 
 function watchTaskStatus() {
 return new Promise((resolve, reject) => {
@@ -557,7 +564,7 @@ $.log('\n🔔每日觀看廣告任務狀態查詢成功,1s後查詢每日觀看�
        resolve()
     })
    })
-  } 
+  }
 
 
 function checkDailyWatchAdId() {
@@ -581,7 +588,7 @@ $.log('\n🔔開始查詢每日觀看廣告ID\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 function DailyWatchAd() {
@@ -612,7 +619,7 @@ $.log('\n🔔開始領取每日觀看獎勵\n')
           resolve()
     })
    })
-  } 
+  }
 
 function checkDailyClickAdId() {
 return new Promise((resolve, reject) => {
@@ -634,7 +641,7 @@ $.log('\n🔔開始查詢每日廣告ID\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 function checkDailyClickAd() {
@@ -658,7 +665,7 @@ $.log('\n🔔開始查詢每日廣告點擊ID\n')
           resolve()
     })
    })
-  } 
+  }
 
 function DailyClickAd() {
 return new Promise((resolve, reject) => {
@@ -681,7 +688,7 @@ $.log('\n🔔開始領取每日點擊獎勵\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 
@@ -734,7 +741,7 @@ $.log('\n🔔首頁金幣狀態:'+checkhomejb.right_text+'\n🔔首頁紅包狀�
           resolve()
     })
    })
-  } 
+  }
 
 
 function homeJin() {
@@ -759,7 +766,7 @@ $.log('\n🔔開始領取首頁金幣\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 
@@ -783,7 +790,7 @@ $.log('\n🔔開始翻倍首頁金幣\n')
           resolve()
     })
    })
-  } 
+  }
 
 function checkRedBagId() {
 return new Promise((resolve, reject) => {
@@ -805,7 +812,7 @@ $.log('\n🔔查詢首頁紅包ID成功,等待30s後領取首頁紅包\n')
           resolve()
     })
    })
-  } 
+  }
 
 function redBagCallback() {
 return new Promise((resolve, reject) => {
@@ -828,7 +835,7 @@ $.log('\n🔔開始領取首頁紅包\n')
           resolve()
     })
    })
-  } 
+  }
 
 function checkGoldEggId() {
 return new Promise((resolve, reject) => {
@@ -854,7 +861,7 @@ $.log('\n🔔開始查詢首頁金蛋ID\n')
           resolve()
     })
    })
-  } 
+  }
 
 function goldEggDone() {
 return new Promise((resolve, reject) => {
@@ -877,7 +884,7 @@ $.log('\n🔔開始領取首頁金蛋獎勵\n')
           resolve()
     })
    })
-  } 
+  }
 
 function goldEggCallback() {
 return new Promise((resolve, reject) => {
@@ -900,7 +907,7 @@ $.log('\n🔔開始翻倍首頁金蛋\n')
           resolve()
     })
    })
-  } 
+  }
 
 function helpStatus() {
 return new Promise((resolve, reject) => {
@@ -921,7 +928,7 @@ $.log('\n🔔今日助力已上限,請明天再試!\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 function checkCode() {
@@ -943,7 +950,7 @@ $.log('\n🔔查詢助力視頻ID成功, 開始觀看助力視頻\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 function helpClick() {
@@ -967,7 +974,7 @@ $.log('\n🔔開始觀看助力視頻, 60s後領取助力視頻獎勵\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 
@@ -992,7 +999,7 @@ $.log('\n🔔開始領取助力視頻獎勵\n')
           resolve()
     })
    })
-  } 
+  }
 
 function getNewsId() {
 return new Promise((resolve, reject) => {
@@ -1020,7 +1027,7 @@ return new Promise((resolve, reject) => {
           resolve()
     })
    })
-  } 
+  }
 
 function autoRead() {
 return new Promise((resolve, reject) => {
@@ -1041,7 +1048,7 @@ return new Promise((resolve, reject) => {
           resolve()
     })
    })
-  } 
+  }
 
 function checkLuckNum() {
 return new Promise((resolve, reject) => {
@@ -1071,7 +1078,7 @@ $.log('\n🔔開始查詢抽獎次數\n')
           resolve()
     })
    })
-  } 
+  }
 
 function luckyClick() {
 return new Promise((resolve, reject) => {
@@ -1097,7 +1104,7 @@ $.log('\n🔔開始抽獎\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 function luckyCallBack() {
@@ -1121,7 +1128,7 @@ $.log('\n🔔開始翻倍抽獎\n')
           resolve()
     })
    })
-  } 
+  }
 
 function luckyBox() {
 return new Promise((resolve, reject) => {
@@ -1147,7 +1154,7 @@ $.log('\n🔔開始打開寶箱\n')
           resolve()
     })
    })
-  } 
+  }
 
 function luckyBoxCallBack() {
 return new Promise((resolve, reject) => {
@@ -1169,7 +1176,7 @@ $.log('\n🔔開始翻倍寶箱\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 
@@ -1201,7 +1208,7 @@ $.log('\n🔔開始查詢答題ID\n')
           resolve()
     })
    })
-  } 
+  }
 
 function checkSp() {
 return new Promise((resolve, reject) => {
@@ -1222,7 +1229,7 @@ return new Promise((resolve, reject) => {
           resolve()
     })
    })
-  } 
+  }
 
 function cySp() {
 return new Promise((resolve, reject) => {
@@ -1242,7 +1249,7 @@ return new Promise((resolve, reject) => {
           resolve()
     })
    })
-  } 
+  }
 
 function answerQue() {
 return new Promise((resolve, reject) => {
@@ -1267,7 +1274,7 @@ $.log('\n🔔開始答題\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 function answerQueCallBack() {
@@ -1291,7 +1298,7 @@ $.log('\n🔔開始翻倍答題金幣\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 function cashCheck() {
@@ -1315,7 +1322,7 @@ if(cash.jinbi >= 500000){
           resolve()
     })
    })
-  } 
+  }
 
 
 
@@ -1339,7 +1346,7 @@ $.log(data)
           resolve()
     })
    })
-  } 
+  }
 
 
 
@@ -1367,7 +1374,7 @@ return new Promise((resolve, reject) => {
       resolve()
     })
    })
-  } 
+  }
 
 
 function doTaskH5() {
@@ -1395,7 +1402,7 @@ $.log('\ndoTaskH5:'+data+'\n')
           resolve()
     })
    })})
-  } 
+  }
 
 function upLoadTime() {
 return new Promise((resolve, reject) => {
@@ -1412,7 +1419,7 @@ $.log('\nupLoadTime:'+timestamp+'\n'+data+'\n')
           resolve()
     })
    })
-  } 
+  }
 
 function upLoadTime2() {
 return new Promise((resolve, reject) => {
@@ -1429,7 +1436,7 @@ $.log('\nupLoadTime2:'+data+'\n')
           resolve()
     })
    })
-  } 
+  }
 
 
 
@@ -1452,7 +1459,7 @@ return new Promise((resolve, reject) => {
           resolve()
     })
    })
-  } 
+  }
 
 
 function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r)));let h=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];h.push(e),s&&h.push(s),i&&h.push(i),console.log(h.join("\n")),this.logs=this.logs.concat(h)}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
